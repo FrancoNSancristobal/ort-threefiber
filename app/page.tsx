de4c5f92aa42bf5d978404e956b511518b15b930
FrancoNSancristobal <francon.sancristobal@gmail.com>
2024-05-11 17:31:28 -0300

    deleteImport

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,20 @@
 import { title, subtitle } from "@/components/primitives";
 import CardsWithProducts from "@/components/cardsWithProducts";
 import CardsNoProduct from "@/components/cardsNoProducts";
-import { Shiba } from "@/components/samsungMonitor";
 
 export default function Home() {
-	return (
-		<section className="flex flex-col items-center justify-center gap-4">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Transformando&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>compras&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					en experiencia.
-				</h1>
-			</div>
-			
-			<div style={{marginTop: '40px'}}>
-                <CardsWithProducts />
-				</div>
-		</section>
-	);
+  return (
+    <section className="flex flex-col items-center justify-center gap-4">
+      <div className="inline-block max-w-lg text-center justify-center">
+        <h1 className={title()}>Transformando&nbsp;</h1>
+        <h1 className={title({ color: "violet" })}>compras&nbsp;</h1>
+        <br />
+        <h1 className={title()}>en experiencia.</h1>
+      </div>
+
+      <div style={{ marginTop: "40px" }}>
+        <CardsWithProducts />
+      </div>
+    </section>
+  );
 }
